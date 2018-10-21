f2060c227354bbf0bc4ea13807268088728869df
dong <dong@dongs-MBP.lan>
2018-10-21 19:46:55 +0800

    loading data into state through onClick

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 // import Header from './Header';
 import Header from './Header';
-import Inventory from './Inventory'
-import Order from './Order'
+import Inventory from './Inventory';
+import Order from './Order';
+
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -22,6 +24,11 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSampleFishes = () => {
+    alert('Loading Sample');
+    this.setState({fishes: sampleFishes});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,7 +36,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" age={500} cool={true}/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
